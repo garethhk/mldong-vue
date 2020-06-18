4bc26f6d43a3d0721eb70bfbc25597187b1ab9c2
menglidong <524719755@qq.com>
2020-06-18 23:21:51 +0800

    修改路由模式hash->history

--- src/router/index.js
+++ src/router/index.js
@@ -81,7 +81,7 @@ export const asyncRoutes = [
   }, hidden: true }
 ]
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  mode: 'history', // require service support
   scrollBehavior: () => ({ y: 0 }),
   routes: [
     ... constantRoutes,
