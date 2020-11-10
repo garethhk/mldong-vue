0f7f7ca6cb0c70c8a09281ff563f8a16050d43bd
mldong <524719755@qq.com>
2020-11-10 20:40:44 +0800

    重写选择树组件-dialog=>下拉

--- src/components/m/SelectTree/index.vue
+++ src/components/m/SelectTree/index.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="m-select-tree">
-    <el-input readonly :size="size" :placeholder="placeholder" v-model="mValue">
-      <el-button slot="append" icon="el-icon-search" @click="openDialog"></el-button>
+  <el-select :value="valueTitle" :clearable="clearable" @clear="clearHandle">
+    <el-input
+      class="selectInput"
+      :size="size"
+      :placeholder="placeholder"
+      v-model="filterText">
     </el-input>
-    <el-dialog :title="dialogTitle" :visible.sync="isOpenDialog" :width="dialogWidth" append-to-body @close="handleCancel">
+
+    <el-option :value="valueTitle" :label="valueTitle" class="options">
       <el-tree
-        :props="defaultProps"
-        :data="treeData"
-        node-key="id"
-        highlight-current
-        :default-expand-all="defaultExpandAll"
-        @current-change="handleCurrentChange"
-        ref="tree">
+        id="tree-option"
+        ref="selectTree"
+        :accordion="accordion"
+        :data="options"
+        :props="props"
+        :node-key="props.value"
+        :default-expanded-keys="defaultExpandedKey"
+        :filter-node-method="filterNode"
+        @node-click="handleNodeClick">
       </el-tree>
-      <div slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="handleSubmit">确 定</el-button>
-        <el-button @click="handleCancel">取 消</el-button>
-      </div>
-    </el-dialog>
-  </div>
+    </el-option>
+  </el-select>
 </template>
+
 <script>
 import request from '@/utils/request'
+
 export default {
   name: 'MSelectTree',
   props: {
@@ -33,10 +37,26 @@ export default {
       type: Boolean,
       default: false
     },
-    // 绑定的值
+    /* 配置项 */
+    props: {
+      type: Object,
+      default: () => {
+        return {
+          value: 'id', // ID字段名
+          label: 'name', // 显示名称
+          children: 'children' // 子级字段名
+        }
+      }
+    },
+    /* 选项列表数据(树形结构的对象数组) */
+    options: {
+      type: Array,
+      default: () => { return [] }
+    },
+    /* 初始值 */
     value: {
-      type: [String, Number, Array],
-      default: undefined
+      type: [Number, String, Array],
+      default: () => { return null }
     },
     multiple: { // 是否多选
       type: Boolean,
@@ -46,131 +66,133 @@ export default {
       type: String,
       default: 'medium'
     },
-    placeholder: { //  占位符
-      type: String,
-      default: '请选择'
-    },
-    dialogTitle: { //  弹窗标题
-      type: String,
-      default: '请选择'
-    },
-    dialogWidth: { // 弹窗宽度
-      type: String,
-      default: '30%'
-    },
-    defaultExpandAll: { // 是否默认展开所有节点
+    /* 可清空选项 */
+    clearable: {
       type: Boolean,
-      default: false
+      default: () => { return true }
     },
-    // 是否显示根节点
-    showRoot: {
+    /* 自动收起 */
+    accordion: {
       type: Boolean,
-      default: true
+      default: () => { return false }
+    },
+    placeholder: {
+      type: String,
+      default: () => { return '检索关键字' }
     }
   },
   data() {
     return {
-      mValue: this.showRoot ? '根节点' : '', // 显示的文本值
-      isOpenDialog: false, // 是否打开弹窗
-      treeData: [], // 树型结构
-      tableData: [], // 原始数据
-      defaultProps: { // elementui树型组件默认属性配置
-        children: 'children',
-        label: 'name'
-      }
+      filterText: '',
+      valueId: this.value, // 初始值
+      valueTitle: '',
+      defaultExpandedKey: []
     }
   },
   watch: {
-    value(n, o) { // 监听父组件值变动，子组件也要变动
-      if (o === undefined || o === 0) {
-        this.refreshView()
-      }
+    value() {
+      this.valueId = this.value
+      this.initHandle()
+    },
+    filterText(val) {
+      this.$refs.selectTree.filter(val)
     }
   },
-  created() {
-    if (this.isEdit) {
-      this.requestData()
+  mounted() {
+    if (this.url) {
+      request({
+        url: this.url,
+        method: 'post',
+        data: {
+          pageNum: 1,
+          pageSize: 10000
+        }
+      }).then(res => {
+        if (res.code === 0) {
+          this.options = this.$util.getTree(res.data.rows)
+        }
+        this.initHandle()
+      })
     }
   },
   methods: {
-    requestData() {
-      if (this.treeData.length) {
-        this.$nextTick(() => {
-          // dom更新完成再设置当前选中项
-          this.refreshView()
+    // 初始化值
+    initHandle() {
+      if (this.valueId) {
+        this.valueTitle = this.$refs.selectTree.getNode(this.valueId).data[this.props.label] // 初始化显示
+        this.$refs.selectTree.setCurrentKey(this.valueId) // 设置默认选中
+        this.defaultExpandedKey = [this.valueId] // 设置默认展开
+      }
+      this.initScroll()
+    },
+    // 初始化滚动条
+    initScroll() {
+      this.$nextTick(() => {
+        const scrollWrap = document.querySelectorAll('.el-scrollbar .el-select-dropdown__wrap')[0]
+        const scrollBar = document.querySelectorAll('.el-scrollbar .el-scrollbar__bar')
+        scrollWrap.style.cssText = 'margin: 0px; max-height: none; overflow: hidden;'
+        scrollBar.forEach(ele => {
+          ele.style.width = 0
         })
-        return
-      }
-      if (this.url) {
-        request({
-          url: this.url,
-          method: 'post',
-          data: {
-            pageNum: 1,
-            pageSize: 10000
-          }
-        }).then(res => {
-          this.tableData = res.data.rows
-          if (res.code === 0) {
-            if (this.showRoot) {
-              this.treeData = [
-                {
-                  id: 0,
-                  name: '根节点',
-                  children: []
-                },
-                // 这里使用工具方法将id/parentId数据结构转成children结构
-                ...this.$util.getTree(res.data.rows)
-              ]
-            } else {
-              this.treeData = [
-                // 这里使用工具方法将id/parentId数据结构转成children结构
-                ...this.$util.getTree(res.data.rows)
-              ]
-            }
-            this.$nextTick(() => {
-              // dom更新完成再设置当前选中项
-              this.refreshView()
-            })
-          }
-        })
-      }
+      })
     },
-    openDialog() { // 打开弹出框
-      this.isOpenDialog = true
-      this.requestData()
+    // 切换选项
+    handleNodeClick(node) {
+      this.valueTitle = node[this.props.label]
+      this.valueId = node[this.props.value]
+      this.$emit('input', this.valueId)
+      this.defaultExpandedKey = []
     },
-    handleSubmit() {
-      this.isOpenDialog = false
+    // 清除选中
+    clearHandle() {
+      this.valueTitle = ''
+      this.valueId = null
+      this.defaultExpandedKey = []
+      this.clearSelected()
+      this.$emit('input', null)
     },
-    handleCancel() {
-      this.isOpenDialog = false
+    /* 清空选中样式 */
+    clearSelected() {
+      const allNode = document.querySelectorAll('#tree-option .el-tree-node')
+      allNode.forEach((element) => element.classList.remove('is-current'))
     },
-    // 处理当前选中节点变化时触发的事件
-    handleCurrentChange(data) {
-      // 修改显示
-      this.mValue = data.name
-      // 子组件值变化要通过父组件
-      this.$emit('input', data.id)
-    },
-    // 刷新页面元素
-    refreshView() {
-      if (this.$refs.tree) {
-        if (this.value === undefined) {
-          this.$refs.tree.setCurrentKey(0)
-        } else {
-          this.$refs.tree.setCurrentKey(this.value)
-        }
-      }
-      if (this.isEdit) {
-        var nodes = this.tableData.filter(item => {
-          return item.id === this.value
-        })
-        if (nodes.length) {
-          this.mValue = nodes[0].name
-        }
-      }
+    filterNode(value, data) {
+      if (!value) return true
+      return data.name.indexOf(value) !== -1
     }
   }
 }
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .el-scrollbar .el-scrollbar__view .el-select-dropdown__item{
+    height: auto;
+    max-height: 274px;
+    padding: 0;
+    overflow: hidden;
+    overflow-y: auto;
+  }
+  .el-select-dropdown__item.selected{
+    font-weight: normal;
+  }
+  ul li >>>.el-tree .el-tree-node__content{
+    height:auto;
+    padding: 0 20px;
+  }
+  .el-tree-node__label{
+    font-weight: normal;
+  }
+  .el-tree >>>.is-current .el-tree-node__label{
+    color: #409EFF;
+    font-weight: 700;
+  }
+  .el-tree >>>.is-current .el-tree-node__children .el-tree-node__label{
+    color:#606266;
+    font-weight: normal;
+  }
+  .selectInput{
+    padding: 0 5px;
+    box-sizing: border-box;
+  }
+</style>
