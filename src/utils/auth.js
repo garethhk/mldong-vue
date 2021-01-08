2ac92e7c2f975ea01c9830efabf0026409c31eaf
mldong <524719755@qq.com>
2021-01-08 09:30:17 +0800

    修改cookies-token-key

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
+const TokenKey = process.env.VUE_APP_ENV ? 'm_token' + process.env.VUE_APP_ENV : 'm_token'
 
 export function getToken() {
   return Cookies.get(TokenKey)
