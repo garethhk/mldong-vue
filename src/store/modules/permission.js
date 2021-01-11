8e5c1dc57ca158926dbb6ca11365437d029377b2
mldong <524719755@qq.com>
2021-01-11 08:16:40 +0800

    权限文件去掉冗余代码

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -2,91 +2,6 @@ import { constantRoutes } from '@/router'
 import { getRouters } from '@/api/sys/sys.rbac.service'
 import Layout from '@/layout/index'
 
-/**
- * Use meta.access to determine if the current user has permission
- * @param menus
- * @param route
- */
-function hasPermission(menus, route) {
-  if (route.name) {
-    var currMenu = getMenu(route.name, menus)
-    if (currMenu !== null) {
-      // 设置菜单的标题、图标
-      if (currMenu.name) {
-        route.meta.title = currMenu.name
-      }
-      if (currMenu.icon) {
-        route.meta.icon = currMenu.icon
-      }
-      if (currMenu.sort) {
-        route.sort = currMenu.sort
-      }
-      if (currMenu.remark) {
-        route.meta.tagName = currMenu.remark
-      }
-      if (currMenu.tagName) {
-        route.meta.tagName = currMenu.tagName
-      }
-    } else {
-      route.sort = 10
-    }
-  }
-  if (route.meta && route.meta.access) {
-    return menus.some(menu => route.meta.access.includes(menu.routeName))
-  } else {
-    return true
-  }
-}
-// 根据路由名称获取菜单
-function getMenu(access, menus) {
-  for (let i = 0; i < menus.length; i++) {
-    var menu = menus[i]
-    if (access === menu.routeName) {
-      return menu
-    }
-  }
-  return null
-}
-// 对菜单进行排序
-// function sortRouters(accessedRouters) {
-//   for (let i = 0; i < accessedRouters.length; i++) {
-//     var router = accessedRouters[i]
-//     if (router.children && router.children.length > 0) {
-//       router.children.sort(compare('sort'))
-//     }
-//   }
-//   accessedRouters.sort(compare('sort'))
-// }
-
-// 升序比较函数
-// function compare(p) {
-//   return function(m, n) {
-//     var a = m[p]
-//     var b = n[p]
-//     return a - b
-//   }
-// }
-/**
- * Filter asynchronous routing tables by recursion
- * @param routes asyncRoutes
- * @param menus
- */
-export function filterAsyncRoutes(routes, menus) {
-  const res = []
-
-  routes.forEach(route => {
-    const tmp = { ...route }
-    if (hasPermission(menus, tmp)) {
-      if (tmp.children) {
-        tmp.children = filterAsyncRoutes(tmp.children, menus)
-      }
-      res.push(tmp)
-    }
-  })
-
-  return res
-}
-
 // 遍历后台传来的路由字符串，转换为组件对象
 function filterAsyncRouter(asyncRouterMap) {
   return asyncRouterMap.filter(route => {
@@ -130,11 +45,6 @@ const actions = {
         commit('SET_ROUTES', accessedRoutes)
         resolve(accessedRoutes)
       })
-      // var accessedRoutes = filterAsyncRoutes(asyncRoutes, menus)
-      // // 对菜单进行排序
-      // sortRouters(accessedRoutes)
-      // commit('SET_ROUTES', accessedRoutes)
-      // resolve(accessedRoutes)
     })
   }
 }
